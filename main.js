b014f0753f4444032fa8dedeb253473512bb4170
Artem <artemzh@santur.ru>
2021-10-13 08:55:42 +0500

    add gitignore

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-console.log("main.js loaded");
+$(document).ready(function(){
+    "use strict";
+    console.log("main.js loaded");
+
+    paper.install(window);
+    paper.setup(document.getElementById("mainCanvas"));
+
+   
+
+    paper.view.draw();
+
+});
