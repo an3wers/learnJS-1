621cb9219240353bb356c3cfcafdb0b0be5695f1
Artem <artemzh@santur.ru>
2021-10-14 09:52:20 +0500

    game

--- main.js
+++ main.js
@@ -1,12 +1,71 @@
-$(document).ready(function(){
-    "use strict";
-    console.log("main.js loaded");
+let funds = 50;
+let round = 0;
 
-    paper.install(window);
-    paper.setup(document.getElementById("mainCanvas"));
+function rand(m, n) { // функция возвращает случайное число
+    return m + Math.floor((n - m + 1) * Math.random());
+}
 
-   
+function randFace() { // случайный элемент массива
+    return ["crown", "anchor", "heart", "spade", "club", "diamond"]
+        [rand(0,5)];
+}
 
-    paper.view.draw();
+while (funds > 1 && funds < 100 ) {
 
-});
+    round++;
+    console.log('round ${round}:');
+    console.log('\tstarting funds: ${funds}p');
+
+    // Размещение ставок
+    
+    const bets = {
+        crown: 0,
+        anchor: 0,
+        heart: 0,
+        spade: 0,
+        club: 0,
+        diamond: 0,
+    };
+
+    let totalBet = rand(1, funds);
+
+    if (totalBet === 7) {
+        totalBet = funds;
+        bets.heart = totalBet;
+    }
+    else {
+        // распределение всех ставок
+        let remaining = totalBet;
+    do {
+        let bet = rand(1, remaining);
+        let face = randFace();
+        bets[face] = bets[face] + bet;
+        remaining = remaining - bet;
+    } while (remaining > 0);
+    }
+
+    funds = funds - totalBet;
+    console.log('\tbets: ' + Object.keys(bets).map(face => '${face}: ${bets[face]} pence').join(', ') + '(total: ${totalBet} pence)');
+
+    // Бросок костей
+ 
+    const hand = [];
+    for (let roll = 0; roll < 3; roll++) {
+        hand.push(randFace());
+    }
+
+    console.log('\thand: ${hand.join(', ')}');
+
+    let winnings = 0;
+    for(let die=0; die < hand.length; die++) {
+        let face = hand[die];
+        if (bets[face] > 0) {
+            winnings = winnings + bets[face];
+        }
+    }
+    funds = funds + winnings;
+        console.log('\twinnings: ${winnings}');
+
+} // закрывается цикл while
+
+console.log('\tending funs: ${funds}');
